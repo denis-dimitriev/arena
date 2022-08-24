fa658a0c6ee135181d8967465dab69a62fb0ba02
win <cryptomining1988@gmail.com>
2022-08-24 21:01:44 +0300

    added firebase to the project

---
+++ src/utils/firebase.config.ts
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCrib9yeSXUMYpI-AeUDFKXd3I4hBe0tQw",
+  authDomain: "arena-5cc60.firebaseapp.com",
+  projectId: "arena-5cc60",
+  storageBucket: "arena-5cc60.appspot.com",
+  messagingSenderId: "372611087366",
+  appId: "1:372611087366:web:dd04c526a2905c43b0d787"
+};
+
+// Initialize Firebase
+export const firebaseApp = initializeApp(firebaseConfig);
